3176aa010f1fb33057817b5628edf9e3b929baa3
Virgil Xu <virgilxu89@gmail.com>
2018-01-13 01:58:54 -0800

    added about-editor

---
+++ src/components/about-editor.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class AboutEditor extends Component {
+  render() {
+    return (
+      <div>
+        <div>const aboutMe = </div>
+        <div>name: 'Virgil Xu',</div>
+        <div>currentLocation: 'San Jose, CA'</div>
+        <div>'resume'</div>
+        <div>email: 'virgilxu89@gmail.com'</div>
+        <div>Github: 'Github'</div>
+        <div>LinkedIn: 'LinkedIn'</div>
+        <div>GradEducation: 'M.S. Engineering with Emphasis in Batteries, San Jose State University'</div>
+        <div>UnderGradEducation: 'B.S. Chemical Engineering, University of California, Davis'</div>
+        <div>skills: ['HTML5', 'CSS3', 'Sass', 'JavaScript', 'ReactJS', 'webpack']</div>
+        <div>}</div>
+      </div>
+    )
+  }
+}
+
+export default AboutEditor;
--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import AboutEditor from './about-editor';
 
 class About extends Component {
   render() {
     return (
-      <div>Hello, World! My name is Virgil Xu, a Front-End Engineer in the Bay Area. I like to work on fun and challenging web applications. Check out my projects!</div>
+      <div>
+        <div>Hello, World! My name is Virgil Xu, a Front-End Engineer in the Bay Area. I like to work on fun and challenging web applications. Check out my projects!</div>
+        <AboutEditor />
+      </div>
     )
   }
 }
