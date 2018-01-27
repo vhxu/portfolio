7773c9a34b4f72c233b70f25e7a1659540919f10
Virgil Xu <virgilxu89@gmail.com>
2018-01-27 13:49:37 -0800

    added a media-query

--- src/data/project-info.js
+++ src/data/project-info.js
@@ -1,7 +1,7 @@
 const projectInfo = [
   {
     title: 'Stock Market',
-    description: 'An easy to use app to look up stocks. This was the first project where I used Redux to handle the application state. The app design was inspired by the Robinhood app. The API used was from IEX, Sparklines was used for the preview graph, Highcharts was used to graph the stock prices, and react-autosuggest was used to look up stock symbols.',
+    description: 'An easy to use app to look up stocks. This was the first project where I used Redux to handle the application state. The app design was inspired by the Robinhood app. The API used was from IEX and Highcharts was used to graph the stock prices.',
     screenshot: "url('../src/images/stock-screenshot-full.png')",
     name: 'stock-market',
     color: '#334CAA',
@@ -25,7 +25,7 @@ const projectInfo = [
   },
   {
     title: 'Calculator',
-    description: 'A copy of the iPhone calculator that we use everyday on our phone. This project was written with only HTML, CSS, and Javascript. It helped me understand how to structure my projects as an MVC. This really helped me build a stronger JS foundation for web development.',
+    description: 'A copy of the iPhone calculator that we use everyday on our phone. This project was written with HTML, CSS, and JavaScript. It helped me understand how to structure my projects as an MVC and build a stronger JavaScript foundation for web development.',
     screenshot: "url('../src/images/calculator-screenshot.png')",
     name: 'calculator',
     color: '#0f61ab',
